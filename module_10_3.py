e52e7ed771f42f8bb97ae2aedd9c9e3a6405b631
Danil Terentev <danil.terentyev@gmail.com>
2024-09-02 15:58:09 +0300

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -6,36 +6,29 @@ import time
 class Bank:
     def __init__(self):
         self.balance = 0
-        self.locker = threading.Lock()
+        self.lock = threading.Lock()
 
     def deposit(self):
-        try:
-            for _ in range(100):
-                money = random.randint(50, 500)
-                with self.locker:
-                    self.balance += money
-                    if self.balance >= 500 and self.locker.locked():
-                        self.locker.release()
-                print(f"Пополнение: {money}. Баланс: {self.balance}")
-                time.sleep(0.001)
-        except Exception as exc:
-            print(f'Ошибка в deposit: {exc}')
+        for i in range(100):
+            value = random.randint(50, 500)
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            self.balance += value
+            print(f'Пополнение: {value}. Баланс: {self.balance}')
+            time.sleep(0.001)
 
     def take(self):
-        try:
-            for _ in range(100):
-                money = random.randint(50, 500)
-                print(f"Запрос на {money}")
-                with self.locker:
-                    if money <= self.balance:
-                        self.balance -= money
-                        print(f"Снятие: {money}. Баланс: {self.balance}")
-                    else:
-                        print("Запрос отклонён, недостаточно средств")
-                        self.locker.acquire()
+        for i in range(100):
+            value = random.randint(50, 500)
+            print(f'Запрос на {value}')
+            if self.balance >= value:
+                self.balance -= value
+                print(f'Снятие: {value}. Баланс: {self.balance}')
                 time.sleep(0.001)
-        except Exception as exc:
-            print(f'Ошибка в take: {exc}')
+            else:
+                print(f'Запрос отклонён, недостаточно средств')
+                time.sleep(0.001)
+                self.lock.acquire()
 
 
 bk = Bank()
